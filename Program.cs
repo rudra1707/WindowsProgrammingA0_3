5626997efeab836099da8025eb256318487003f3
rudra <bhattrudra17072005@gmail.com>
2024-10-18 20:00:00 -0400

    Initialization of data structures and creation of GUID and then creating test data

--- Program.cs
+++ Program.cs
@@ -42,3 +42,90 @@ class Program
             Console.ReadKey();
             return;
         }
+        // Initialize data structures to store GUID
+        var stringArray = new string[numElements];
+        var stringList = new List<string>(numElements);
+        var stringDictionary = new Dictionary<string, string>(numElements);
+
+        // Create random GUIDs and fill in data structures.
+        GenerateRandomGUIDs(numElements, stringArray, stringList, stringDictionary);
+
+        // Generate test data to search
+        var (validTestData, invalidTestData) = GenerateTestData(numTestElements, stringArray);
+
+        // Running the performance test
+        TestPerformance("Array", stringArray, validTestData, invalidTestData, (data, key) => Array.Find(data, e => e == key));
+        TestPerformance("List", stringList, validTestData, invalidTestData, (data, key) => data.BinarySearch(key) >= 0 ? key : null);
+        TestPerformance("Dictionary", stringDictionary, validTestData, invalidTestData, (data, key) => data.ContainsKey(key) ? data[key] : null);
+
+        // This Displays command line arguments
+        Console.WriteLine($"Command Line Arguments: Elements = {numElements}, Test Elements = {numTestElements}");
+
+        // Waits for the user to press any key to close the console
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    // Method for creating random GUIDs and filling the given data structures
+    static void GenerateRandomGUIDs(int numElements, string[] stringArray, List<string> stringList, Dictionary<string, string> stringDictionary)
+    {
+        for (int i = 0; i < numElements; i++)
+        {
+            string guid = Guid.NewGuid().ToString();
+            stringArray[i] = guid;
+            stringList.Add(guid);
+            stringDictionary[guid] = guid;
+        }
+    }
+
+    // Method to provide both valid and false test data for searching
+    static (string[], string[]) GenerateTestData(int numTestElements, string[] stringArray)
+    {
+        Random rand = new Random();
+        var validTestData = new string[numTestElements];
+        var invalidTestData = new string[numTestElements];
+
+        for (int i = 0; i < numTestElements; i++)
+        {
+            validTestData[i] = stringArray[rand.Next(0, stringArray.Length)];
+            invalidTestData[i] = Guid.NewGuid().ToString(); // Generate new random GUIDs for invalid test data
+        }
+
+        return (validTestData, invalidTestData);
+    }
+
+    // Method to evaluate different data structures' performance
+
+    static void TestPerformance<T>(
+    string structureName,
+    T dataStructure,
+    string[] validTestData,
+    string[] invalidTestData,
+    Func<T, string, string> searchFunc)
+    {
+        // Calculating time for valid searches
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        foreach (var key in validTestData)
+        {
+            searchFunc(dataStructure, key);
+        }
+        stopwatch.Stop();
+        double validTotalTime = stopwatch.Elapsed.TotalMilliseconds;
+        double validAverageTime = validTotalTime / validTestData.Length;
+
+        // Calculating time for invalid searches
+        stopwatch.Restart();
+        foreach (var key in invalidTestData)
+        {
+            searchFunc(dataStructure, key);
+        }
+        stopwatch.Stop();
+        double invalidTotalTime = stopwatch.Elapsed.TotalMilliseconds;
+        double invalidAverageTime = invalidTotalTime / invalidTestData.Length;
+
+        // Displaying results
+        Console.WriteLine($"{structureName} - Total time for valid data: {validTotalTime} ms, Average time per search: {validAverageTime} ms");
+        Console.WriteLine($"{structureName} - Total time for invalid data: {invalidTotalTime} ms, Average time per search: {invalidAverageTime} ms");
+    }
+}
